31418ae1557c88463a744e6555547520ccaa85c5
LeJohne <johngoy11024@gmail.com>
2021-01-31 23:41:51 -0500

    finished

--- src/ProblemSet9.java
+++ src/ProblemSet9.java
@@ -1,42 +1,193 @@
+import java.util.*;
 public class ProblemSet9 {
-
-    public int[] evenOdd(int[] arr) {
-
+    public  int[] evenOdd(int[] arr) {
+        int [] sequence = new int[arr.length];
+        if( arr == null ){
+            return null;
+        } else {
+            int count = 0;
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] % 2 == 0 ){
+                    sequence[count]= arr[i];
+                    count++;
+                }
+            }
+            for( int j = 0; j < arr.length; j++){
+                if(arr[j] % 2 == 1 ){
+                    sequence[count] = arr[j];
+                    count++;
+                }
+            }
+            return sequence;
+        }
     }
 
     public int[] notAlone(int[] arr, int alone) {
-
+        if(arr == null || arr.length == 0){
+            return null;
+        }
+        for(int i = 0; i < arr.length; i++){
+            if (arr[i] == alone){
+                if(arr[i-1] > arr[i+1]){
+                    arr[i] = arr[i-1];
+                }else if (arr[i+1] > arr[i-1]){
+                    arr[i] = arr[i+1];
+                }
+            }
+        }
+        return arr;
     }
 
-    public int[] shiftLeft(int[] arr) {
+    public  int[] shiftLeft(int[] arr) {
+        int [] shift = new int[arr.length];
 
+        if( arr == null || arr.length == 0 ){
+            return null;
+        } else{
+            for( int i = 1; i < arr.length; i++){
+                shift[ i-1] = arr[i];
+            }
+            shift[arr.length-1] = arr[0];
+            return shift;
+        }
     }
 
     public int[] fillIn(int start, int end) {
+        int count = 0;
+        int[] output = new int[end - start];
 
+        if (start <= end) {
+            for (int i = start; i <= end - 1; i++) {
+                output[count] = i;
+                count++;
+            }
+
+        }
+        return output;
     }
 
-    public boolean triple(int[] arr) {
+    public  boolean triple(int[] arr) {
+        if( arr == null ){
+            return false;
+        } else {
+            int count = 0;
+            for(int i = 0; i <arr.length; i++){
+                if( arr[i] == 3){
+                    count++;
+                }
+            }
+            if(count == 3) {
+                return true;
+            } else{
+                return false;
+            }
+
+        }
 
     }
 
     public int pairs(int[] a, int[] b) {
-
+        int n;
+        if (a == null || b == null || a.length != b.length) {
+            return -1;
+        }
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            n = a[i] - b[i];
+            if (n <= 2 && n >= -2 && n != 0) {
+                count++;
+            }
+        }
+        return count;
     }
 
-    public boolean twentyFour(int[] arr) {
-
+    public  boolean twentyFour(int[] arr) {
+        if (arr == null) {
+            return false;
+        } else {
+            int two = 0;
+            int four = 0;
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] == 2 && arr[i - 1] == 2) {
+                    two++;
+                }
+                if (arr[i] == 4 && arr[i - 1] == 4) {
+                    four++;
+                }
+            }
+            if (two >= 1 && four >= 1) {
+                return false;
+            } else if (four >= 1) {
+                return true;
+            } else if (two >= 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
-
     public boolean fourteen(int[] arr) {
-
+        if (arr == null) {
+            return false;
+        } else {
+            int remove = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] != 1 && arr[i] != 4) {
+                    remove++;
+                }
+            }
+            if (remove == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
-    public int centeredAverage(int[] arr) {
-
+    public  int centeredAverage(int[] arr) {
+        if(arr == null || arr.length < 3){
+            return -1;
+        }
+        int max = arr[0];
+        int min = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        int subtract = 0;
+        int sum = 0;
+        for(int k = 0; k < arr.length; k++){
+            sum +=arr[k];
+            if(arr[k] == max || arr[k] == min){
+                subtract+= arr[k];
+            }
+        }
+        sum = sum - subtract;
+        int avg = sum / ((arr.length - 2));
+        return avg;
     }
 
-    public int outliers(int[] arr) {
+    public  int outliers(int[] arr) {
+        if (arr == null || arr.length < 1) {
+            return -1;
+        } else {
+            int minimum= arr[0];
+            int maximum=arr[0];
 
+            for (int i = 0; i <arr.length ; i++) {
+                if( arr[i] > maximum){
+                    maximum = arr[i];
+                }
+                if(arr[i] < minimum){
+                    minimum = arr[i];
+                }
+            }
+
+            return maximum - minimum;
+        }
     }
 }
---
+++ src/Testing.java
@@ -0,0 +1,46 @@
+//import java.util.Arrays;
+//
+//public class Testing {
+//    public static void main(String[] args) {
+//        ProblemSet9 ps = new ProblemSet9();
+//        int[] iarr = {};
+//        boolean b = false;
+//        int i = 0;
+//        String s = "";
+//
+//        int[] input1 = {2, 10, 7, 2};
+//        int[] input2 = {1, 2, 3};
+//        int num = 2;
+//
+////Example 1
+////          iarr = ps.evenOdd(input1);
+////          System.out.print(Arrays.toString(iarr));
+////Example 2
+////            iarr = ps.notAlone(input1, num);
+////            System.out.print(Arrays.toString(iarr));
+//////Example 3
+////            iarr = ps.shiftLeft(input1);
+////            System.out.print(Arrays.toString(iarr));
+////Example 4
+////            iarr = ps.fillIn(5,10);
+////            System.out.print(Arrays.toString(iarr));
+//////Example 5
+////            b = ps.triple(input1);
+////            System.out.print("\n"+b);
+//////Example 6
+////            i = ps.pairs(input1, input2);
+////            System.out.print(i);
+//////Example 7
+////            b = ps.twentyFour(input1);
+////            System.out.print("\n" + b );
+//////Example 8
+////            b = ps.fourteen(input1);
+////            System.out.print("\n" + b);
+////Example 9
+////            i = ps.centeredAverage(input1);
+////            System.out.print("\n" + i);
+//////Example 10
+//            i = ps.outliers(input1);
+//            System.out.print("\n" + i);
+//    }
+//}
